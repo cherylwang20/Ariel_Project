8f5707c85d589a820c8e94ea17250ee0f32965e7
HUIYI WANG <90352960+cherylwang20@users.noreply.github.com>
2023-04-16 15:22:22 -0400

    4/16 3:22

--- Ariel_target.py
+++ Ariel_target.py
@@ -8,4 +8,13 @@ column_names = df.columns
 new_target = df[df['Planet Period [days]'] < 5]
 
 df.to_csv('ariel_target.csv')
-print(new_target)
+
+
+JWST_Cycle1 = pd.read_csv("All JWST transiting exoplanet observations  (GTO+GO+ERS) - All Transiting Exoplanet Observations.csv"
+                          ,skiprows= 7)
+
+print(len(JWST_Cycle1['Observation']))
+
+JWST_Phase = JWST_Cycle1[JWST_Cycle1['Observation'] == 'PHASE']
+print(JWST_Phase)
+print(f'The total JWST cycle 1 timeframe devoted to Phase Curve Observation is ~{11/141*100:.3f}%')
